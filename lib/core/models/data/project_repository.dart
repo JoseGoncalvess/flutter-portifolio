5a0dcf73e45bbcb19d48b1e3e498873cfa29d5d0
José Gonçalves <joseinacio774@gmail.com>
2025-05-18 20:15:09 -0300

    feat: model service github

---
+++ lib/core/models/data/project_repository.dart
@@ -0,0 +1,15 @@
+class ProjectRepository {
+  final String name;
+  final String decription;
+  final String img;
+  final String path;
+  final List<String> languages;
+
+  ProjectRepository({
+    required this.name,
+    required this.decription,
+    required this.img,
+    required this.path,
+    required this.languages,
+  });
+}
---
+++ lib/core/services/git_hub_service.dart
@@ -0,0 +1,2 @@
+// URL_BASE = "https://api.github.com/users/JoseGoncalvess/repos";
+// / /URL_LANGAGES= "https://api.github.com/repos/JoseGoncalvess/Card-credit/languages"
